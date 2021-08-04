4072df76c31408209bcc013d2a5468029ea6592e
kczssd <511124893@qq.com>
2021-08-04 16:53:43 +0800

    add:更换个人头像

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import {getUserProfile} from '@tarojs/taro'
 import { Label, Radio, RadioGroup, View,Image, Picker } from '@tarojs/components'
 import styles from './index.module.less'
 import GoodsItem from '../../modules/goods/index'
@@ -16,7 +17,8 @@ export default () => {
   const {MachineID,setMachineID} = useInfo();
   const Types = ["水", "罐装", "果汁", "牛奶", "茶", "酒"];
   const { goods, setgoods } = useGoods();
-  
+  const [Avatar,setAvater] = useState(person)
+
   async function getGoods(): Promise<void> {
     let { data: { inventories } } = await request({
       method: "POST",
@@ -31,11 +33,18 @@ export default () => {
   return (
     <View className={styles.mainPage}>
       <View className={styles.head}>
-        <Image mode="aspectFit" src={person} className={styles.person}></Image>
+        <Image mode="aspectFit" onClick={()=>{
+          getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+              const {userInfo:{avatarUrl}} = res;
+              setAvater(avatarUrl)
+            }
+          })
+        }} src={Avatar} className={styles.person}></Image>
         <View>
           <Picker className={styles.picker} range={positionArray} onChange={(e)=>{
             setMachineID(e.detail.value as string)
-            // getGoods();
           }}>
             <Image mode="aspectFit" src={location} className={styles.positionIcon}></Image>
             <View className={styles.position}>{positionArray[parseInt(MachineID)]}  &gt;</View>
